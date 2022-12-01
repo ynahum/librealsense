d8a0e00464aa851e68f219966dd2665c5de34178
Eran <librealsense.eran@gmail.com>
2022-12-01 17:06:39 +0200

    remove poc-[eh].h

--- tools/dds/poc/e/poc-e.cpp
+++ tools/dds/poc/e/poc-e.cpp
@@ -1,8 +1,6 @@
 // License: Apache 2.0. See LICENSE file in root directory.
 // Copyright(c) 2022 Intel Corporation. All Rights Reserved.
 
-#include "poc-e.h"
-
 #include <realdds/dds-participant.h>
 #include <realdds/dds-utilities.h>
 #include <realdds/dds-time.h>
