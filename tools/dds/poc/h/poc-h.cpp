2791817f85ff6864b6568d4df83d2190274ae243
ynahum <yair.y.nahum@intel.com>
2022-12-09 00:59:13 +0200

    add streams enable mask + dump + fix max/min calc + ignore first time sync iteration

--- tools/dds/poc/h/poc-h.cpp
+++ tools/dds/poc/h/poc-h.cpp
@@ -19,10 +19,42 @@
 #include <tclap/UnlabeledValueArg.h>
 
 
+using namespace std;
 using realdds::dds_nsec;
 using realdds::dds_time;
 using realdds::timestr;
 
+enum class stream_enable_flags : uint32_t {
+    RGB = 0x1,
+    DEPTH = 0x2,
+    GYRO = 0x4,
+    IMU = 0x8,
+    SAFETY = 0x10
+};
+
+struct stream_stats_data
+{
+    uint64_t count = 0;
+    uint64_t drops = 0;
+    uint64_t last_number;
+    realdds::dds_nsec total_transit_nsec = 0;
+    realdds::dds_nsec max_transit_nsec, min_transit_nsec;
+    realdds::dds_time first, last;
+};
+
+
+static inline bool is_stream_enabled(uint32_t enable_mask, stream_enable_flags stream_bit_flag) {
+    uint32_t flag = static_cast<uint32_t>(stream_bit_flag);
+    return ((enable_mask & flag) != 0);
+}
+
+void stream_stats_data_dump(const string& stream_name, const stream_stats_data& data ) {
+
+    LOG_INFO("stream " << stream_name << " stats:");
+    LOG_INFO("  count: " << data.count);
+    LOG_INFO("  drops: " << data.drops);
+    LOG_INFO("  last_number: " << data.last_number);
+}
 
 dds_nsec
 calc_time_offset( poc::op_writer & h2e, poc::op_reader & e2h, int const n_reps = 10 )
@@ -55,13 +87,16 @@ calc_time_offset( poc::op_writer & h2e, poc::op_reader & e2h, int const n_reps =
         auto time_offset = ( t1 - t0 + t2 - t3 );
         time_offset /= 2;
         LOG_DEBUG( "   time-offset= " << timestr( time_offset, timestr::rel ) << "    round-trip= " << timestr( t3_, t0_ ) );
-        avg_time_offset += time_offset;
+        
+        // ignore first iteration
+        if (i > 0)
+            avg_time_offset += time_offset;
     }
     //
     // NOTE: the time-offset is what needs to be added to the HOST timestamp in order to arrive at the EMBEDDED time.
     // Obviously, negating it will yield the other direction...
     //
-    avg_time_offset /= -n_reps;
+    avg_time_offset /= (n_reps-1);
     LOG_INFO( "Average time-offset= " << timestr( avg_time_offset, timestr::rel ) );
     return avg_time_offset;
 }
@@ -73,9 +108,11 @@ int main( int argc, char** argv ) try
     TCLAP::SwitchArg debug_arg( "", "debug", "Enable debug logging", false );
     TCLAP::ValueArg< realdds::dds_domain_id > domain_arg( "d", "domain", "select domain ID to listen on", false, 0, "0-232" );
     TCLAP::SwitchArg op_pub_sub_arg( "o", "op-pub-sub", "create op pub and sub", false );
-    TCLAP::ValueArg< int > time_sync_iter_arg( "s", "time-sync", "number of iterations", false, 10, "0-inf" );
-    TCLAP::UnlabeledValueArg< std::string > command_arg( "command", "command to send", false, "", "string" );
-    TCLAP::ValueArg< int > stream_run_time( "r", "run-time", "streaming time in seconds", false, 30, "0-inf" );
+    TCLAP::ValueArg< uint32_t > time_sync_iter_arg( "s", "time-sync", "number of iterations", false, 10, "0-inf" );
+    TCLAP::UnlabeledValueArg< string > command_arg( "command", "command to send", false, "", "string" );
+    TCLAP::ValueArg< uint32_t > stream_run_time( "t", "run-time", "streaming time in seconds", false, 30, "0-inf" );
+    TCLAP::ValueArg< uint32_t > streams_enable_mask(
+        "m", "streams-enable-mask", "streams mask to enable", false, static_cast<uint32_t>(stream_enable_flags::DEPTH), "0-inf" );
 
     cmd.add(domain_arg);
     cmd.add(debug_arg);
@@ -83,7 +120,7 @@ int main( int argc, char** argv ) try
     cmd.add(op_pub_sub_arg);
     cmd.add(time_sync_iter_arg);
     cmd.add(stream_run_time);
-    
+    cmd.add(streams_enable_mask);
     cmd.parse( argc, argv );
 
     // Configure the same logger as librealsense, and default to only errors by default...
@@ -160,16 +197,7 @@ int main( int argc, char** argv ) try
 #endif
     }
 
-    struct framedata
-    {
-        uint64_t count = 0;
-        uint64_t drops = 0;
-        uint64_t last_number;
-        realdds::dds_nsec total_transit_nsec = 0;
-        realdds::dds_nsec max_transit_nsec, min_transit_nsec;
-        realdds::dds_time first, last;
-    };
-    auto process_frame = [time_offset]( std::shared_ptr< framedata > const & fdata,
+    auto process_frame = [time_offset]( shared_ptr< stream_stats_data > const & fdata,
                                         poc::stream_reader::data_t const & mdata ) {
         auto number = mdata.msg._frame_number;
         //
@@ -181,9 +209,9 @@ int main( int argc, char** argv ) try
         auto transit_nsec = mdata.sample.reception_timestamp.to_ns()                  // in our time domain
                           - ( mdata.sample.source_timestamp.to_ns() + time_offset );  // in the embedded time domain
         fdata->total_transit_nsec += transit_nsec;
-        if( ! fdata->count || fdata->max_transit_nsec > transit_nsec )
+        if( ! fdata->count || fdata->max_transit_nsec < transit_nsec )
             fdata->max_transit_nsec = transit_nsec;
-        if( ! fdata->count || fdata->min_transit_nsec < transit_nsec )
+        if( ! fdata->count || fdata->min_transit_nsec > transit_nsec )
             fdata->min_transit_nsec = transit_nsec;
         //
         // time spread, so we can average
@@ -196,25 +224,85 @@ int main( int argc, char** argv ) try
         fdata->last_number = number;
     };
 
-    using namespace std::placeholders;  // _1, etc...
+    using namespace placeholders;  // _1, etc...
 
-    LOG_INFO( "   create depth stream reader");
-    poc::stream_reader depth( participant, "realsense/depth" );
-    auto depth_data = std::make_shared< framedata >();
-    LOG_INFO( "   register depth callback on_data");
-    depth.on_data( std::bind( process_frame, depth_data, _1 ));
-    LOG_INFO( "   wait for writers");
-    depth.wait_for_writers( 1, std::chrono::seconds( 300 ) );
+    uint8_t num_of_running_streams = 0;
+
+    shared_ptr<poc::stream_reader> depth;
+    shared_ptr<poc::stream_reader> rgb;
+    shared_ptr<poc::stream_reader> gyro;
+    shared_ptr<poc::stream_reader> imu;
+    shared_ptr<poc::stream_reader> safety;
+    auto depth_data = make_shared< stream_stats_data >();
+    auto rgb_data = make_shared< stream_stats_data >();
+    auto gyro_data = make_shared< stream_stats_data >();
+    auto imu_data = make_shared< stream_stats_data >();
+    auto safety_data = make_shared< stream_stats_data >();
+
+    uint8_t streams_mask = streams_enable_mask.getValue();
+    LOG_INFO( "   streams_mask: " << hex << streams_mask);
+    if (is_stream_enabled(streams_mask, stream_enable_flags::DEPTH)) {
+        LOG_INFO( "   create depth stream reader");
+        depth = make_shared<poc::stream_reader>( participant, "realsense/depth" );
+        num_of_running_streams++;
+        depth->on_data( bind( process_frame, depth_data, _1 ));
+        depth->wait_for_writers( 1, chrono::seconds( 300 ) );
+    }
+
+    if (is_stream_enabled(streams_mask, stream_enable_flags::RGB)) {
+        LOG_INFO( "   create rgb stream reader");
+        rgb = make_shared<poc::stream_reader>( participant, "realsense/rgb" );
+        num_of_running_streams++;
+        rgb->on_data( bind( process_frame, rgb_data, _1 ));
+        rgb->wait_for_writers( 1, chrono::seconds( 300 ) );
+    }
+
+    if (is_stream_enabled(streams_mask, stream_enable_flags::GYRO)) {
+        LOG_INFO( "   create gyro stream reader");
+        gyro = make_shared<poc::stream_reader>( participant, "realsense/gyro" );
+        num_of_running_streams++;
+        gyro->on_data( bind( process_frame, gyro_data, _1 ));
+        gyro->wait_for_writers( 1, chrono::seconds( 300 ) );
+    }
+
+    if (is_stream_enabled(streams_mask, stream_enable_flags::IMU)) {
+        LOG_INFO( "   create imu stream reader");
+        imu = make_shared<poc::stream_reader>( participant, "realsense/imu" );
+        num_of_running_streams++;
+        imu->on_data( bind( process_frame, imu_data, _1 ));
+        imu->wait_for_writers( 1, chrono::seconds( 300 ) );
+    }
+
+    if (is_stream_enabled(streams_mask, stream_enable_flags::SAFETY)) {
+        LOG_INFO( "   create safety stream reader");
+        safety = make_shared<poc::stream_reader>( participant, "realsense/safety" );
+        num_of_running_streams++;
+        safety->on_data( bind( process_frame, safety_data, _1 ));
+        safety->wait_for_writers( 1, chrono::seconds( 300 ) );
+    }
 
     // Collect frame data
-    LOG_INFO( "   got writers. streaming should start");
+    LOG_INFO( "   streaming should start if enabled");
 
-    LOG_INFO( "   main thread goes to sleep for " << stream_run_time.getValue() << " seconds");
+    LOG_INFO( "   main thread goes to sleep for " << dec << stream_run_time.getValue() << " seconds");
     std::this_thread::sleep_for( std::chrono::seconds( stream_run_time.getValue() ) );
 
     // Dump it all out somehow
-    LOG_INFO( "   TODO: another time offset check and compare");
-    LOG_INFO( "   TODO: dump data somehow");
+
+    if (is_stream_enabled(streams_mask, stream_enable_flags::DEPTH))
+        stream_stats_data_dump("DEPTH", *depth_data);    
+
+    if (is_stream_enabled(streams_mask, stream_enable_flags::RGB))
+        stream_stats_data_dump("RGB", *rgb_data);    
+
+    if (is_stream_enabled(streams_mask, stream_enable_flags::GYRO))
+        stream_stats_data_dump("GYRO", *gyro_data);    
+
+    if (is_stream_enabled(streams_mask, stream_enable_flags::IMU))
+        stream_stats_data_dump("IMU", *imu_data);    
+
+    if (is_stream_enabled(streams_mask, stream_enable_flags::SAFETY))
+        stream_stats_data_dump("SAFETY", *safety_data);    
 
     return EXIT_SUCCESS;
 }
